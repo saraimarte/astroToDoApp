e91ae4799a654e79e5ad07448a9fd84afd9352f0
Sarai Marte <smartepujols@gmail.com>
2024-01-02 19:00:56 -0500

    had to npm install typescript and add api keys in env

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,4 +3,12 @@ declare namespace App {
   interface Locals {
     email: string;
   }
+  interface ImportMetaEnv {
+    readonly SUPABASE_URL: string
+    readonly SUPABASE_ANON_KEY: string
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
 }
